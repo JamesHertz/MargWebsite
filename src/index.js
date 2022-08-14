626afc1663c03e7618529b1e8be95fb0c00a12c1
jhertz <jh.furtado@fct.unl.pt>
2022-08-15 00:16:01 +0100

    check files implemented

--- src/index.js
+++ src/index.js
@@ -15,24 +15,91 @@ function fetchOK(url, options) {
     });
 }
 
+
+
+class InfoPane {
+    constructor() {
+        this.file = elt('em')
+        this.dom = elt('div', { id: 'editor-info' }, this.file)
+    }
+
+    syncState(state) {
+        this.file.textContent = state.selFile
+    }
+}
+
+class Editor {
+
+    constructor() {
+        this.dom = elt('textarea', { id: 'editor' })
+    }
+
+    syncState({ selFile }) {
+        if (this.selFile === selFile) return
+        fetchOK(`/${selFile}`).then(
+            res => res.text()
+        ).then(txt =>
+            this.dom.textContent = txt
+        ).catch(console.log)
+        this.selFile = selFile
+    }
+}
+
+
+class FilePane {
+
+    // by now
+    constructor(files, dispatch) {
+        this.files = []
+        this.dom = elt('div', { id: 'file-pane' },
+            ...files.map(
+                file => elt('button', {
+                    className: 'file',
+                    // animation ...
+                    onclick: () => {
+                        dispatch({ selFile: file })
+                    }
+                }, file)
+            )
+        )
+    }
+
+    syncState() { }
+
+}
+
+
+class MargDisplay {
+    constructor(parent, files, state) {
+        let components = [new Editor(files), new InfoPane()]
+        let filePane = new FilePane(files, update_state)
+
+        function update_state(diff) {
+            state = Object.assign(state, diff)
+            components.forEach(e => e.syncState(state))
+        }
+
+
+        this.dom = elt(
+            'div', { id: 'main-pane' },
+            filePane.dom,
+            elt('div', { id: 'editor-pane' },
+                ...components.map(c => c.dom)
+            )
+        )
+        parent.appendChild(this.dom)
+        update_state({ selFile: files[0] })
+    }
+}
+
 async function getFiles() {
     let res = await fetchOK('/')
     return (await res.text()).split('\n')
 }
 
 async function runApp() {
-
-    let dom = elt(
-        'div', { id: 'main-frame' },
-        elt('div', { id: 'file-pane' },
-            ...(await getFiles()).map(
-                f => elt('div', { className: 'file' }, f, elt('br'))
-            )
-        )
-        ,
-        elt('textarea', { id: 'editor' })
-    )
-    document.body.appendChild(dom)
+    let files = await getFiles()
+    new MargDisplay(document.body, files, {})
 }
 
 runApp()
