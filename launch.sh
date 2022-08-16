3e798ebe01ee5dcb7999fac1ac526f3ff867f01e
jhertz <jh.furtado@fct.unl.pt>
2022-08-16 21:25:00 +0100

    little change in script file

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ SRC_DIR='src'
 
 echo "use the following http://localhost:8000/index.html to have access to the website"
 if [[ -e $START_DIR ]]; then
-	rm -r $START_DIR/*
+	rm -rf $START_DIR/*
 else
 	mkdir $START_DIR
 fi
