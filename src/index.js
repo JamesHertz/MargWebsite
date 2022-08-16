7caef87528f9b8bb1ef9aa9b1eb693718e55aecc
jhertz <jh.furtado@fct.unl.pt>
2022-08-16 13:43:40 +0100

    delete and save implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+function handleAction(state, action) {
+    if (!action.type) return Object.assign(state, action)
+    switch (action.type) {
+        case 'delete': {
+            let { selectedFile, files } = state 
+            files = files.filter(file => file != selectedFile)
+            fetchOK(`/${selectedFile}`, { method: 'DELETE' }).catch(console.log)
+            return Object.assign(state, { selectedFile: files[0], files })
+        }
+        case 'save': {
+            let { selectedFile, fileContent } = state
+            fetchOK(`/${selectedFile}`, {
+                body: fileContent, method: 'PUT'
+            }).catch(console.log)
+        }
+            break;
+
+    }
+    return state
+}
+
+function fetchOK(url, options) {
+    return fetch(url, options).then(response => {
+        if (response.status < 400) return response;
+        else throw new Error(response.statusText);
+    });
+}
+
 function elt(type, props, ...children) {
     let dom = document.createElement(type);
     if (props) Object.assign(dom, props);
@@ -8,50 +36,12 @@ function elt(type, props, ...children) {
     return dom;
 }
 
-function fetchOK(url, options) {
-    return fetch(url, options).then(response => {
-        if (response.status < 400) return response;
-        else throw new Error(response.statusText);
-    });
-}
-
-
-
-class InfoPane {
-    constructor() {
-        this.file = elt('em')
-        this.dom = elt('div', { id: 'editor-info' }, this.file)
-    }
-
-    syncState(state) {
-        this.file.textContent = state.selected
-    }
-}
-
-class Editor {
-
-    constructor() {
-        this.dom = elt('textarea', { id: 'editor' })
-    }
-
-    syncState({ selected }) {
-        if (this.selected === selected) return
-        fetchOK(`/${selected}`).then(
-            async res => {
-                this.dom.value = await res.text()
-                this.dom.scrollTop = 0 // detail
-                this.selected = selected 
-            }
-        ).catch(console.log)
-    }
-}
-
-
 function renderFile(file, dispatch) {
     return elt('div',
         {
             className: 'file',
-            onclick: () => dispatch({ selected: file })
+            onclick: () =>
+                dispatch({ selectedFile: file })
         }, file
     )
 }
@@ -65,7 +55,7 @@ class FilePane {
         this.fileComps = Object.create(null)
     }
 
-    syncState({files, selected}){
+    syncState({ files, selectedFile }) {
         if (this.files != files) {
             this.dom.textContent = ''
             this.dom.append(...files.map(
@@ -78,28 +68,110 @@ class FilePane {
             this.files = files
         }
 
-        if(this.selected != selected){
-            if(this.selected)
-                this.fileComps[this.selected].classList.remove('selected')
-            this.fileComps[selected].classList.add('selected')
-            this.selected = selected
+        if (this.selectedFile != selectedFile) {
+            if (this.selectedFile)
+                this.fileComps[this.selectedFile].classList.remove('selected')
+            this.fileComps[selectedFile].classList.add('selected')
+            this.selectedFile = selectedFile
         }
     }
 
 }
 
+class Editor {
+
+    constructor(dispatch) {
+        let dom = elt('textarea', { id: 'editor' })
+        dom.addEventListener('change', () => {
+            dispatch({fileContent: dom.value})
+        })
+        this.dom = dom
+        this.dispatch = dispatch
+    }
+
+    syncState({ selectedFile }) {
+        if (this.selectedFile === selectedFile) return
+        fetchOK(`/${selectedFile}`).then(
+            async res => {
+                let fileContent = await res.text()
+                this.dom.value = fileContent
+                this.dom.scrollTop = 0 // detail
+                this.selectedFile = selectedFile
+                this.dispatch({fileContent})
+            }
+        ).catch(console.log)
+    }
+}
+
+
+class InfoPane {
+    constructor() {
+        this.dom = elt('em')
+        //this.dom = elt('div', { id: 'editor-info' }, this.file)
+    }
+
+    syncState(state) {
+        this.dom.textContent = state.selectedFile
+    }
+}
+
+
+class Delete {
+    constructor(dispatch) {
+        this.dom = elt(
+            'button',
+            {
+                className: 'edit-button',
+                onclick: () => dispatch({ type: 'delete' })
+            },
+            'delete'
+
+        )
+    }
+
+    syncState() { }
+
+}
+
+class Save {
+    constructor(dispatch) {
+        this.dom = elt(
+            'button',
+            {
+                className: 'edit-button',
+                onclick: () => dispatch({ type: 'save' })
+            },
+            'save'
+        )
+    }
+
+    syncState() { }
+}
 
 class MargApp {
     constructor(init_state, dispatch) {
         let filePane = new FilePane(dispatch)
-        let editcomp = [new Editor(), new InfoPane()]
-        this.components = [filePane, ...editcomp]
+        let editor = new Editor(dispatch)
+        let editTools = [InfoPane, Save, Delete]
+
+        this.components = [filePane, editor]
 
         this.dom = elt(
             'div', { id: 'main-pane' },
             filePane.dom,
             elt('div', { id: 'editor-pane' },
-                ...editcomp.map(c => c.dom)
+                editor.dom,
+                elt('div',
+                    { id: 'edit-tools' },
+                    ...editTools.map(
+                        builder => {
+                            let tool = new builder(dispatch)
+                            this.components.push(tool)
+                            return tool.dom
+                        }
+                    )
+
+                )
             )
         )
         this.syncState(init_state)
@@ -118,9 +190,9 @@ async function getFiles() {
 
 function runApp() {
     getFiles().then(files => {
-        let state = { files, selected: files[0] }, app;
+        let state = { files, selectedFile: files[0] }, app;
         app = new MargApp(state, function (action) {
-            state = Object.assign(state, action)
+            state = handleAction(state, action)
             app.syncState(state)
         })
         document.body.appendChild(app.dom)
